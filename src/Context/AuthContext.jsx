515917a505cae4f2b560748c4be3d92caadfb754
githua976sys <githua976@gmail.com>
2026-05-21 10:25:49 +0300

    Add AuthContext.jsx

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,62 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import { auth } from "../firebase/firebase";
+
+const AuthContext = createContext();
+
+const provider = new GoogleAuthProvider();
+
+export const AuthProvider = ({ children }) => {
+
+  const [user, setUser] = useState(null);
+
+  // Google Login
+  const googleSignIn = async () => {
+    await signInWithPopup(auth, provider);
+  };
+
+  // Logout
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  // Detect Logged User
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (currentUser) => {
+        setUser(currentUser);
+      }
+    );
+
+    return () => unsubscribe();
+
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        googleSignIn,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
