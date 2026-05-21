04be7505f2e3790874f263ea5b3bac0fd4d0938d
githua976sys <githua976@gmail.com>
2026-05-21 21:18:14 +0300

    update AppRoutes.jsx

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,35 +1,71 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 import MainLayout from "../Components/Layout/MainLayout";
 
-
-
-
-
-
 import LoginPage from "../Pages/LoginPage";
+
 import AboutusPage from "../Pages/AboutusPage";
+
 import Home from "../Pages/HomePage";
+
 import FootballPage from "../Pages/FootballPage";
+
 import Favorites from "../Pages/FavoritesPage";
+
 import Music from "../Pages/MusicPage";
 
-
 const App = () => {
+
   return (
+
     <BrowserRouter>
+
       <Routes>
-        <Route element ={<MainLayout/>}>
-         <Route path="/" element ={<Home/>}/>
-          <Route path="/Music" element ={<Music/>}/>
-          <Route path="/Football" element ={<FootballPage/>}/>
-          <Route path="/Favorites" element ={<Favorites/>}/>
-          <Route path="/About" element ={<AboutusPage/>}/>
-          <Route path="/login" element={<LoginPage />} />
+
+        {/* Login Route */}
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+
+        {/* Dashboard Layout */}
+        <Route element={<MainLayout />}>
+
+          <Route
+            path="/"
+            element={<Home />}
+          />
+
+          <Route
+            path="/music"
+            element={<Music />}
+          />
+
+          <Route
+            path="/football"
+            element={<FootballPage />}
+          />
+
+          <Route
+            path="/favorites"
+            element={<Favorites />}
+          />
+
+          <Route
+            path="/about"
+            element={<AboutusPage />}
+          />
+
         </Route>
+
       </Routes>
+
     </BrowserRouter>
-    
+
   );
 };
 
