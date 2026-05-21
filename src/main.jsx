8fd7ca274f2729dfd6c3f4cab71bea7c08053d9a
githua976sys <githua976@gmail.com>
2026-05-21 11:20:11 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./Context/AuthContext";
+
 import { PlayerProvider } from "./Context/PlayerContext";
 import { FavoritesProvider } from "./Context/FavoriteContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
+    <AuthProvider>              
       <FavoritesProvider>
         <PlayerProvider>
           <App />
