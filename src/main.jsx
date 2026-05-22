531aa231d0602bdabac35f155f92b9d8e588f082
githua976sys <githua976@gmail.com>
2026-05-22 07:48:26 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,18 @@ import "./index.css";
 import { PlayerProvider } from "./Context/PlayerContext";
 import { FavoritesProvider } from "./Context/FavoriteContext";
 import { AuthProvider } from "./Context/AuthContext";
+import { ThemeProvider } from "./Context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>              
-      <FavoritesProvider>
-        <PlayerProvider>
-          <App />
-        </PlayerProvider>
-      </FavoritesProvider>
-    </AuthProvider>
-
+    <ThemeProvider>
+      <AuthProvider>
+        <FavoritesProvider>
+          <PlayerProvider>
+            <App />
+          </PlayerProvider>
+        </FavoritesProvider>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
